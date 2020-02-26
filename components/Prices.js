7ae9ad966f31e0d93b278da4fb5125d29231485c
tyson <tyelf22@hotmail.com>
2020-02-25 22:14:11 -0700

    dynamically evaluate state

--- components/Prices.js
+++ components/Prices.js
@@ -3,43 +3,17 @@ import { useState } from 'react'
 
 const Prices = (props) => {
     const [currency, setCurrency] = useState('USD')
-
-    let list = ''
-
-    if(currency === 'USD') {
-        list =  <li className="list-group-item">
-                    Bitcoin rate for {props.bpi.USD.description}
-                    : <span className="badge badge-primary">
-                        {props.bpi.USD.code}
-                        </span>
-                        {' '}
-                        <strong>{props.bpi.USD.rate}</strong>
-                </li>
-    } else if(currency === 'GBP'){
-        list =  <li className="list-group-item">
-                    Bitcoin rate for {props.bpi.GBP.description}
-                    : <span className="badge badge-primary">
-                        {props.bpi.GBP.code}
-                        </span>
-                        {' '}
-                        <strong>{props.bpi.GBP.rate}</strong>
-                </li>
-
-    } else if(currency === 'EUR'){
-        list =  <li className="list-group-item">
-                    Bitcoin rate for {props.bpi.EUR.description}
-                    : <span className="badge badge-primary">
-                        {props.bpi.EUR.code}
-                        </span>
-                        {' '}
-                        <strong>{props.bpi.EUR.rate}</strong>
-                </li>
-    }
-
     return (
         <div>
             <ul className="list-group">
-                {list} 
+                <li className="list-group-item">
+                    Bitcoin rate for {props.bpi[currency].description}
+                    : <span className="badge badge-primary">
+                        {props.bpi[currency].code}
+                        </span>
+                        {' '}
+                        <strong>{props.bpi[currency].rate}</strong>
+                </li>
             </ul>
             <br/>
             <select onChange={e => setCurrency(e.target.value)} 
